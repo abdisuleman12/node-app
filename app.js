58c56643d67258044ab74c80060a55237ac8d9f7
Abdirahman Suleman <abdirahmansuleman@Abdirahmans-MacBook-Pro.local>
2018-12-07 16:42:47 -0600

    added debug

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express')
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path')
 
 const app = express();
 
+app.use(morgan('tiny'));
+
 app.get('/', function(req, res) {
-    
+    res.sendFile(path.join(__dirname,'views', 'index.html'))
 })
+
+app.listen(3000, function() {
+   debug(`listening on port ${chalk.green('3000')}`);
+});
